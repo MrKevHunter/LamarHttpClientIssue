e6861bb70352c64676b54be88e1a71b7b23cc6da
khunter@empactis.com <khunter@empactis.com>
2021-06-09 09:13:10 +0100

    remove unneeded files

--- Startup.cs
+++ Startup.cs
@@ -25,7 +25,7 @@ namespace clean
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-   //         services.AddHttpClient();
+            services.AddHttpClient();
             services.AddRazorPages();
         }
 
